465d8504a3a6b6f1c5d762c72a05fc0bb170e71f
voidism <b05901033@ntu.edu.tw>
2024-05-18 21:06:13 -0400

    fixed suggestions from @amyeroberts on Mar 28

--- tests/generation/test_utils.py
+++ tests/generation/test_utils.py
@@ -1248,6 +1248,7 @@ class GenerationTesterMixin:
             # enable cache if the model is not openai-gpt, xlnet, cpm, or xlm
             config, input_ids, attention_mask, _ = self._get_input_ids_and_config(batch_size=1)
 
+            # Some models don't support the cache and returning past_key_values
             if not hasattr(config, "use_cache"):
                 config.use_cache = False
             else:
